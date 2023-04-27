44bb4c04012daf682b840aca96e5450da4c9fc16
Youri Kopecky <youri@kasual.biz>
2023-04-27 19:20:36 +0200

    make calls.ts testable and fix filters with operators issues

--- src/utils/calls.ts
+++ src/utils/calls.ts
@@ -13,6 +13,104 @@ import { Endpoint } from '../enums/endpoint.enum';
 import { PrestashopNodeAPIResponse } from '../types/calls.type';
 
 /**
+ * Generate url SearchParams.
+ *
+ * @param params
+ * @returns
+ */
+export const generateURLSearchParams = (
+  params: GetAllParams | GetParams | PostParams | PutParams
+): URLSearchParams => {
+  const searchParams = new URLSearchParams();
+
+  /** Display params */
+  if (Array.isArray(params.display)) {
+    searchParams.set('display', `[${params.display.join(',')}]`);
+  } else {
+    searchParams.set('display', params.display || 'full');
+  }
+
+  return searchParams;
+};
+
+/**
+ * Format display, limit, skip, filters and sort into
+ * a URLSearchParams that will be usable by prestashop's
+ * web services.
+ *
+ * @param params
+ * @returns
+ */
+export const generateGetAllURLSearchParams = (
+  params: GetAllParams
+): URLSearchParams => {
+  const searchParams = generateURLSearchParams(params);
+
+  /** Limit */
+  if (params.limit && params.skip) {
+    searchParams.set('limit', `${params.skip},${params.limit}`);
+  } else if (params.limit) {
+    searchParams.set('limit', params.limit.toString());
+  }
+
+  /** Filters */
+  if (params.filters) {
+    params.filters.forEach((filter: Filter, i: number) => {
+      const key = `filter[${filter.key}]`;
+      const hasMultipleFiltersWithTheSameKey =
+        (params.filters?.filter((item) => item.key === filter.key) || [])
+          .length > 1;
+
+      /**
+       * If there are multiple filters with the same key we apply the OR operator of prestashop.
+       * Otherwise we set the filter normally.
+       * Example: [{ key: "id", value: 1 }, { key: "id", value: 2 }]
+       * will become filter[id]=[1|2]
+       **/
+      if (hasMultipleFiltersWithTheSameKey && i > 0) {
+        searchParams.set(
+          key,
+          `[${searchParams.get(key)?.replace(/\[|\]/g, '')}|${filter.value}]`
+        );
+      } else {
+        searchParams.set(key, `[${filter.value}]`);
+      }
+
+      /**
+       * If there are no multiple filters with the same key, we apply the operator.
+       * Prestashop's webservices don't handle the combination of % and OR.
+       **/
+      if (!hasMultipleFiltersWithTheSameKey) {
+        const initializedFilter = searchParams.get(key);
+
+        switch (filter.operator) {
+          case 'start':
+            searchParams.set(key, `%${initializedFilter}`);
+            break;
+
+          case 'end':
+            searchParams.set(key, `${initializedFilter}%`);
+            break;
+
+          case 'contains':
+            searchParams.set(key, `%${initializedFilter}%`);
+            break;
+
+          default:
+            break;
+        }
+      }
+    });
+  }
+
+  if (params.sort) {
+    searchParams.set('sort', `[${params.sort.toString()}]`);
+  }
+
+  return searchParams;
+};
+
+/**
  * Handle the listing of entities on prestashop with filtering and pagination.
  *
  * @param path
@@ -28,75 +126,7 @@ export const getAllCall = async <T>(
     path: `/${endpoint}`,
     paramsSerializer: {
       serialize: (serializeParams) => {
-        const searchParams = new URLSearchParams();
-
-        /** Display params */
-        if (Array.isArray(params.display)) {
-          searchParams.set('display', `[${params.display.join(',')}]`);
-        } else {
-          searchParams.set('display', params.display || 'full');
-        }
-
-        /** Limit */
-        if (params.limit && params.skip) {
-          searchParams.set('limit', `${params.skip},${params.limit}`);
-        } else if (params.limit) {
-          searchParams.set('limit', params.limit.toString());
-        }
-
-        /** Filters */
-        if (params.filters) {
-          params.filters.forEach((filter: Filter) => {
-            const key = `filter[${filter.key}]`;
-            const filterExists = !!searchParams.get(key);
-
-            /**
-             * If there are multiple filters with the same key we apply the OR operator of prestashop.
-             * Otherwise we set the filter normally.
-             * Example: [{ key: "id", value: 1 }, { key: "id", value: 2 }]
-             * will become filter[id]=[1|2]
-             **/
-            if (filterExists) {
-              searchParams.set(
-                key,
-                `[${searchParams.get(key)?.replace(/\[|\]/g, '')}|${
-                  filter.value
-                }]`
-              );
-            } else {
-              searchParams.set(key, `[${filter.value}]`);
-            }
-
-            /**
-             * If there are no multiple filters with the same key, we apply the operator.
-             * Prestashop's webservices don't handle the combination of % and OR.
-             **/
-            if (!filterExists) {
-              const initializedFilter = searchParams.get(key);
-
-              switch (filter.operator) {
-                case 'start':
-                  searchParams.set(key, `%${initializedFilter}`);
-                  break;
-
-                case 'end':
-                  searchParams.set(key, `${initializedFilter}%`);
-                  break;
-
-                case 'contains':
-                  searchParams.set(key, `%${initializedFilter}%`);
-                  break;
-
-                default:
-                  break;
-              }
-            }
-          });
-        }
-
-        if (params.sort) {
-          searchParams.set('sort', `[${params.sort.toString()}]`);
-        }
+        const searchParams = generateGetAllURLSearchParams(params);
 
         return `${qs.stringify(serializeParams)}&${searchParams.toString()}`;
       },
