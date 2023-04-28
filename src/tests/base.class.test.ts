8d33cc3cd201d68fe43c25c3c0f89c75a0f34419
Youri Kopecky <youri@kasual.biz>
2023-04-28 17:44:08 +0200

    test: add test limit

--- src/tests/base.class.test.ts
+++ src/tests/base.class.test.ts
@@ -16,53 +16,64 @@ describe('Base', async () => {
     mockHTTPCalls();
   });
 
-  test('Should have getAll, get, create and update properties', () => {
-    const products = new Base<Product>(Endpoint.products);
+  describe('getAll', () => {
+    test('Should have getAll, get, create and update properties', () => {
+      const products = new Base<Product>(Endpoint.products);
 
-    expect(products).toHaveProperty('getAll');
-    expect(products).toHaveProperty('get');
-    expect(products).toHaveProperty('create');
-    expect(products).toHaveProperty('update');
-  });
-
-  test('Should list all products with all properties', async () => {
-    const products = new Base<Product>(Endpoint.products);
-    const { data } = await products.getAll();
-
-    expect(data).toStrictEqual(mockProducts);
-  });
-
-  test('Should list all products id', async () => {
-    const products = new Base<Product>(Endpoint.products);
-    const { data } = await products.getAll({ display: ['id'] });
-
-    expect(data).toStrictEqual(mockProductsOnlyID);
-  });
-
-  test('Should filter on id', async () => {
-    const products = new Base<Product>(Endpoint.products);
-    const { data } = await products.getAll({
-      filters: [{ key: 'id', value: 1 }],
+      expect(products).toHaveProperty('getAll');
+      expect(products).toHaveProperty('get');
+      expect(products).toHaveProperty('create');
+      expect(products).toHaveProperty('update');
     });
 
-    expect(data).toStrictEqual(mockProductsOnlyIDEquals1);
-  });
+    test('Should list all products with all properties', async () => {
+      const products = new Base<Product>(Endpoint.products);
+      const { data } = await products.getAll();
 
-  test('Should filter on name containing "ora"', async () => {
-    const products = new Base<Product>(Endpoint.products);
-    const { data } = await products.getAll({
-      filters: [{ key: 'name', value: 'ora', operator: 'contains' }],
+      expect(data).toStrictEqual(mockProducts);
     });
 
-    expect(data).toStrictEqual(mockProductsOnlyNameContainsOra);
-  });
+    test('Should list all products id', async () => {
+      const products = new Base<Product>(Endpoint.products);
+      const { data } = await products.getAll({ display: ['id'] });
 
-  test('Should sort by id DESC', async () => {
-    const products = new Base<Product>(Endpoint.products);
-    const { data } = await products.getAll({
-      sort: ['id_DESC'],
+      expect(data).toStrictEqual(mockProductsOnlyID);
     });
 
-    expect(data).toStrictEqual(mockProductsIdDesc);
+    test('Should filter on id', async () => {
+      const products = new Base<Product>(Endpoint.products);
+      const { data } = await products.getAll({
+        filters: [{ key: 'id', value: 1 }],
+      });
+
+      expect(data).toStrictEqual(mockProductsOnlyIDEquals1);
+    });
+
+    test('Should filter on name containing "ora"', async () => {
+      const products = new Base<Product>(Endpoint.products);
+      const { data } = await products.getAll({
+        filters: [{ key: 'name', value: 'ora', operator: 'contains' }],
+      });
+
+      expect(data).toStrictEqual(mockProductsOnlyNameContainsOra);
+    });
+
+    test('Should sort by id DESC', async () => {
+      const products = new Base<Product>(Endpoint.products);
+      const { data } = await products.getAll({
+        sort: ['id_DESC'],
+      });
+
+      expect(data).toStrictEqual(mockProductsIdDesc);
+    });
+
+    test('Should limit to 10 items', async () => {
+      const products = new Base<Product>(Endpoint.products);
+      const { data } = await products.getAll({
+        limit: 10,
+      });
+
+      expect(data?.length).toStrictEqual(10);
+    });
   });
 });
