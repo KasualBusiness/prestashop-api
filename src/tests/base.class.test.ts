eb92721e036aaeebdd8de1a44fa43dae2101399f
Youri Kopecky <youri@kasual.biz>
2023-04-28 16:54:05 +0200

    test: add tests some tests for base class

--- src/tests/base.class.test.ts
+++ src/tests/base.class.test.ts
@@ -1,9 +1,14 @@
-import { describe, test, expect } from 'vitest';
+import { describe, test, expect, beforeAll } from 'vitest';
 import { Product } from '..';
 import { Base } from '../classes/base.class';
 import { Endpoint } from '../enums/endpoint.enum';
+import { mockProducts, mockHTTPCalls, mockProductsOnlyID } from './mocks';
 
 describe('Base', async () => {
+  beforeAll(() => {
+    mockHTTPCalls();
+  });
+
   test('Should have getAll, get, create and update properties', () => {
     const products = new Base<Product>(Endpoint.products);
 
@@ -11,5 +16,19 @@ describe('Base', async () => {
     expect(products).toHaveProperty('get');
     expect(products).toHaveProperty('create');
     expect(products).toHaveProperty('update');
+  });
+
+  test('Should list all products with all properties', async () => {
+    const products = new Base<Product>(Endpoint.products);
+    const { data } = await products.getAll();
+
+    expect(data).toStrictEqual(mockProducts);
+  });
+
+  test('Should list all products id', async () => {
+    const products = new Base<Product>(Endpoint.products);
+    const { data } = await products.getAll({ display: ['id'] });
+
+    expect(data).toStrictEqual(mockProductsOnlyID);
   });
 });
